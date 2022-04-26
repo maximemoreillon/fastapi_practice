5a72d0b66e8a29397d477c842585766c49d0e5fe
Maxime Moreillon <m.moreillon@gmail.com>
2022-04-26 19:07:05 +0900

    added uvicorn in main

--- main.py
+++ main.py
@@ -20,3 +20,8 @@ app.include_router(fruits.router, prefix="/fruits")
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
